20654e314b2e609e761d98e75577615482538ee5
Vinton Teo <taulago@ymail.com>
2019-04-13 05:05:02 -0600

    Add to submitTask function so that tasks also push corresponding object into global array of tasks appended in the aside. clearAll function also clears tasks array.

--- main.js
+++ main.js
@@ -3,25 +3,29 @@ const taskItemInput = document.querySelector('.task-form__inner-input');
 const taskForm = document.querySelector('.task-form');
 const uTaskList = document.querySelector('ul');
 const clearAllBtn = document.querySelector('#clear-btn');
+const standbyTasks = [];
 
-taskForm.addEventListener('click', submitTask);
+taskForm.addEventListener('submit', submitTask);
 clearAllBtn.addEventListener('click', clearAll);
 
 function submitTask(e) {
   var randId = Date.now();
+  var listItem = `<li class="list__item"><input id="${randId}" type="checkbox"><label for="${randId}">${taskItemInput.value}</label></li>`;
   e.preventDefault();
-  if (e.target.className === 'task-form__inner-btn') {
-    uTaskList.innerHTML += `<li class="list__item">
-            <input id="${randId}" type="checkbox"><label for="${randId}">${taskItemInput.value}</label>
-          </li>`;
-    clearTaskInput();
-  }
+  var taskItem = {
+    key: randId,
+    task: taskItemInput.value,
+    done: false
+  };
+  standbyTasks.push(taskItem);
+  uTaskList.innerHTML += listItem;
+  clearTaskInput();
 }
 
 function clearAll() {
-  console.log('clearing');
   document.querySelector('.task-form').reset();
   uTaskList.innerHTML = '';
+  standbyTasks.length = 0;
 }
 
 function clearTaskInput() {
