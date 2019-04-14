e7bbda83fb04a3bcfa37281dc909fc7d548272ea
Vinton Teo <taulago@ymail.com>
2019-04-13 20:09:50 -0600

    Create function to append tasks when create task button is clicked.

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const taskItemInput = document.querySelector('.task-form__inner-input');
 const taskForm = document.querySelector('.task-form');
 const uTaskList = document.querySelector('ul');
 const clearAllBtn = document.querySelector('#clear-btn');
+const makeTaskBtn = document.querySelector('#create-card-btn');
+const mainElement = document.querySelector('main');
 const standbyTasks = [];
+const taskCards = [];
 
 taskForm.addEventListener('submit', submitTask);
 taskForm.addEventListener('click', removeTask);
+makeTaskBtn.addEventListener('click', createTask)
 clearAllBtn.addEventListener('click', clearAll);
 
 function submitTask(e) {
@@ -45,5 +49,41 @@ function removeTask(e) {
       }
     }
     e.target.closest('li').remove();
+  }
+}
+
+function createTask() {
+  if (taskTitleInput.value.length < 1 && standbyTasks.length < 1) {
+    alert('You must enter a Task Title and create a list of tasks!');
+  } else {
+    var taskCard = new ToDoList(Date.now(), taskTitleInput.value, standbyTasks);
+    taskCards.push(taskCard);
+    mainElement.insertAdjacentHTML('afterbegin', `<article class="card" data-id="${taskCard.id}">
+      <header class="card-header">${taskCard.title}</header>
+      <section class="card-body">
+        <ul class="card-body__ul">
+        </ul>
+      </section>
+      <footer class="card-footer">
+        <div class="card-footer__img--wrap">
+          <img class="card-footer__img" src="check-yo-self-icons/urgent.svg">
+          <p class="card-footer__img--text">
+            URGENT
+          </p>
+        </div>
+        <div class="card-footer__img--wrap">
+          <img class="card-footer__img" src="check-yo-self-icons/delete.svg">
+          <p class="card-footer__img--text">
+            DELETE
+          </p>
+        </div>
+      </footer>
+    </article>` );
+    var dataIdKey = `[data-id = "${taskCard.id}"]`;
+    var targetCard = document.querySelector(dataIdKey);
+     console.log(targetCard.childNodes[3].childNodes[1]);
+    standbyTasks.reverse().forEach(function(e) {
+      targetCard.childNodes[3].childNodes[1].insertAdjacentHTML('afterbegin', `<li><input class="checkbox" id="${taskCard.id}" type="checkbox"><label for="${taskCard.id}">${e.task}</label></li>`);
+    })
   }
 }
--- todo-list.js
+++ todo-list.js
@@ -2,7 +2,7 @@ class ToDoList {
   constructor(id, title, tasks, urgent) {
     this.id = id;
     this.title = title;
-    this.tasks = tasks;
+    this.tasks = tasks
     this.urgent = urgent || false;
   }
   saveToStorage() {
